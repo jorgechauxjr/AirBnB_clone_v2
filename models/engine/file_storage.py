3a8cd9bdee8f4124835bcf2af3d5a83e8924894e
jorgechauxjr <chauxjorge@hotmail.com>
2020-04-01 00:00:45 +0000

    Add Task 5.Delete - update filestorage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,19 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None)
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        if cls is None:
+            return self.__objects
+        dict = {}
+        for key, value in self.__objects.items():
+            if value.__class__ == cls:
+                dict[key] = value
+        return dict
+
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +62,11 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it’s inside
+        """
+        if obj is not None:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]

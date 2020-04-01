3f41a78fd5f78052baee8dcd137d1dc5e5b92114
arvicrin <1231@holbertonschool.com>
2020-04-01 03:48:30 +0000

    add the imports at the begining

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -11,11 +13,9 @@ import os
 
 
 if (os.getenv("HBNB_TYPE_STORAGE") == "db"):
-    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
 storage.reload()

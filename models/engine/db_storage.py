21b1590e612fdf8a93db39f9b344e78af306bb13
arvicrin <1231@holbertonschool.com>
2020-04-01 15:41:53 +0000

    Fix the name Base.metadata.drop_all()

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -32,7 +32,7 @@ class DBStorage:
                 os.getenv("HBNB_MYSQL_HOST"),
                 os.getenv("HBNB_MYSQL_DB")), pool_pre_ping=True)
         if os.getenv("HBNB_ENV") == "test":
-            Base.metabase.drop_all(self_engine)
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """show all the instances"""

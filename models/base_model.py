bf467716ae19f5a7c67a1d4a5c7590d40b0fd4be
arvicrin <1231@holbertonschool.com>
2020-04-01 03:45:48 +0000

    Add if a new instances in database

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
-    id = Column(String(60), nullable=False, unique=True, primary_key=True)
+    id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
@@ -33,6 +33,14 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+
+            if self.id is None:
+                setattr(self, 'id', str(uuid.uuid4()))
+
+            if self.created_at is None:
+                self.created_at = datetime.now()
+            if self.updated_at is None:
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()

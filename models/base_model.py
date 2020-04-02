77fb549409f545c01b691aa8aa702f22d4c43d78
arvicrin <1231@holbertonschool.com>
2020-04-02 22:32:02 +0000

    Update basemodel and delete the tescases dbstorage file

--- models/base_model.py
+++ models/base_model.py
@@ -28,17 +28,13 @@ class BaseModel:
             updated_at: updated date
         """
         if kwargs:
+            if self.id is None:
+                setattr(self, 'id', str(uuid.uuid4()))
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-            if self.id is None:
-                setattr(self, 'id', str(uuid.uuid4()))
-            if self.created_at is None:
-                self.created_at = datetime.now()
-            if self.updated_at is None:
-                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()

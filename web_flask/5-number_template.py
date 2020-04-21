3a4663712b9b410bc9fe48e3f4c0706caccc3743
jorgechauxjr <chauxjorge@hotmail.com>
2020-04-21 15:41:29 +0000

    Add from  flask import render_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -9,7 +9,7 @@
     /python/(<text>): Displays 'Python' followed by the value of <text>.
     /number/<n>: display n is a number only if n is an integer
 """
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -40,18 +40,12 @@ def number(n):
     return "{} is a number".format(n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-
-@app.route("/number/<int:n>")
-def number(n):
-    """Display n is a number only if n is an integer"""
-    return "{} is a number".format(n)
-
-
 @app.route("/number_template/<int:n>")
 def template(n):
     """Display a HTML page only if n is an integer:
         H1 tag: Number: n inside the tag BODY"""
     return render_template("5-number.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

8fbd81b01a92bdc0a90843f0f3cf351720f8d3b3
jorgechauxjr <chauxjorge@hotmail.com>
2020-04-22 00:28:57 +0000

    Mod 7-states list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,22 +4,21 @@
 
 from flask import Flask, render_template
 from models import storage
-
+from models.state import State
+from operator import getitem
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/states_list")
-def states_list():
-    """Display  a HTML page: (inside the tag BODY)"""
-    return render_template("7-states_list.html", states=storage.all("State"))
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """List all the states to the client"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """Call in this method storage.close()"""
+def close_db(db):
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
